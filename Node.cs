533158d407835eff4b56e67cc5267cb1f9d06a6b
unknown <vasanthgamer2000@gmail.com>
2023-07-26 17:09:21 +0530

    Changes has been Committed

---
+++ Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section4._7b
+{
+    public class Node
+    {
+        private string data;
+        private Node next = null;
+        public string Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+        public Node Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section4._7b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+            Console.ReadKey();
+        }
+
+        public static void runApp()
+        {
+            SingleLinkedList list = new SingleLinkedList();
+            Node n = new Node();
+            n.Data = "root";
+            list.add(n);
+            for (int i = 0; i < 10; i++)
+            {
+                Node node = new Node();
+                node.Data = Convert.ToString(i);
+                list.add(node);
+            }
+
+            Console.WriteLine("length of singly linked list=" + list.getLengthOfList());
+
+            n = list.getRoot();
+            while (n != null)
+            {
+                Console.WriteLine(n.Data);
+                n = list.getNext();
+            }
+        }
+
+    }
+}
